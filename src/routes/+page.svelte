6010afd8ebbd5a64df3aa8064457cfdf0ee10203
broxdeez <broxdeez@gmail.com>
2023-05-30 17:39:25 -0700

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,134 @@
+<script>
+
+</script>
+<body>
+    <nav>
+        <input type="text" placeholder="search">
+        <input type="password" placeholder="password">
+        <button>Log In</button>
+        <button id='signup'>Sign Up</button>
+    </nav>
+    <main>
+        <h1>Popular Posts</h1>
+        <section class='post'>
+            <span>Posted by <a href="#213adas">/u/not_funny</a></span>
+            <h2>Lol look at this hilarious meme <span class='tags'>funny</span></h2>
+            <button>+Vote</button>
+            <hr>
+        </section>
+        <section class='post'>
+            <span>Posted by <a href="#345adas">/u/gooner4lyfe</a></span>
+            <h2>Happy birthday to the strongest defender/bodyguard in the Prem! <span class='tags'>gunners</span></h2>
+            <button>+Vote</button>
+            <hr>
+        </section>
+        <section class='post'>
+            <span>Posted by <a href="#981adas">/u/dogfan</a></span>
+            <h2>This Corgi got some good stuff from the vet. <span class='tags'>dogs</span></h2>
+            <button>+Vote</button>
+            <hr>
+        </section>
+        <section class='post'>
+            <span>Posted by <a href="#213adas">/u/not_funny</a></span>
+            <h2>Lol look at this hilarious meme <span class='tags'>funny</span></h2>
+            <button>+Vote</button>
+            <hr>
+        </section>
+        <section class='post'>
+            <span>Posted by <a href="#345adas">/u/gooner4lyfe</a></span>
+            <h2>Happy birthday to the strongest defender/bodyguard in the Prem! <span class='tags'>gunners</span></h2>
+            <button>+Vote</button>
+            <hr>
+        </section>
+        <section class='post'>
+            <span>Posted by <a href="#981adas">/u/dogfan</a></span>
+            <h2>This Corgi got some good stuff from the vet. <span class='tags'>dogs</span></h2>
+            <button>+Vote</button>
+            <hr>
+        </section>
+        <section class='post'>
+            <span>Posted by <a href="#213adas">/u/not_funny</a></span>
+            <h2>Lol look at this hilarious meme <span class='tags'>funny</span></h2>
+            <button>+Vote</button>
+            <hr>
+        </section>
+        <section class='post'>
+            <span>Posted by <a href="#345adas">/u/gooner4lyfe</a></span>
+            <h2>Happy birthday to the strongest defender/bodyguard in the Prem! <span class='tags'>gunners</span></h2>
+            <button>+Vote</button>
+            <hr>
+        </section>
+        <section class='post'>
+            <span>Posted by <a href="#981adas">/u/dogfan</a></span>
+            <h2>This Corgi got some good stuff from the vet. <span class='tags'>dogs</span></h2>
+            <button>+Vote</button>
+            <hr>
+        </section>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt iure architecto, tempora nostrum corrupti dicta praesentium fugit velit aliquid magnam, doloremque reiciendis repudiandae! Beatae explicabo cupiditate nobis! Vel, nulla sapiente?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab perspiciatis dolore ipsum modi quisquam voluptas quasi praesentium dolorum id quod aspernatur sit rem illo reiciendis est dolor, cum aut voluptate.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit pariatur, eius porro dolores voluptatum vero nesciunt id earum magni qui alias, voluptate enim iure in possimus reiciendis tempore deserunt saepe.
+        </p>
+        <hr>
+    </main>
+    <footer>
+        <nav>
+            <ul>
+                <li>
+                    <a href="#home">Home</a>
+                </li>
+                <li>
+                    <a href="#about">About</a>
+                </li>
+                <li>
+                    <a href="#contact">Contact</a>
+                </li>
+                <li>
+                    <a href="#puppies">Puppies</a>
+                </li>
+            </ul>
+        </nav>
+    </footer>
+
+</body>
+<style>
+    :global(body) {
+        background-color: #faedcd;
+    }
+
+    #signup {
+        background-color: #ffc300 ;
+    }
+
+    .tags {
+        color: #f72585;
+    }
+    li a {
+        color : #ffafcc;
+    }
+
+    input[type='password'] {
+        color : red;
+    }
+
+    a:hover{
+        color: #457b9d;
+    }
+
+    .post {
+        background-color: #ccd5ae;
+    }
+
+    .post:nth-of-type(2n) {
+        background-color:#e9edc9;
+    }
+    p {
+        font-weight: 600;
+    }
+    p::selection {
+        color : #9e0059;
+    }
+</style>
+
+
+
